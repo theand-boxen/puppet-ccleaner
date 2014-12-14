45210e7d56ef9daf3289675485d73d03a054a75e
Heesang Chae <theand@gmail.com>
2014-12-14 21:15:18 +0900

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class ccleaner {
   package { 'CCleaner':
     provider => 'appdmg',
-    source   => 'http://download.piriform.com/mac/CCMacSetup107.dmg',
+    source   => 'http://download.piriform.com/mac/CCMacSetup109.dmg',
   }
 }
